aff9f6911d2af59115d8c7dec4d93853d28094ac
namhuynn1911 <namhuynh191193@gmail.com>
2022-09-23 08:12:08 +0700

    update

--- 09_aop_exception/exercise/01_book_loan_app/src/main/java/com/book_loan_app/config/LogAspect.java
+++ 09_aop_exception/exercise/01_book_loan_app/src/main/java/com/book_loan_app/config/LogAspect.java
@@ -2,36 +2,36 @@ package com.book_loan_app.config;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Aspect;;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Component
 @Aspect
 public class LogAspect {
+private int count=0;
 
 
-
-    @Pointcut("execution(* com.book_loan_app.controller.BookManagerController.saveBorrowBook(..))")
-    public void borrowBookPointCut() {
+    @Pointcut("execution(* com.book_loan_app.controller.BookManagerController*(..))")
+    public void allControllerMethod() {
     }
 
-    @AfterReturning("borrowBookPointCut()")
-    public void borrowBookMethod(JoinPoint joinPoint) {
+    @AfterReturning("allControllerMethod()")
+    public void afterReturningMethod(JoinPoint joinPoint) {
+        count++;
+        System.out.println("number of hits :"+count);
         System.out.println("mehtod name: "
                 + joinPoint.getSignature().getName()
                 + " time: " + LocalDateTime.now());
     }
 
-    @Pointcut("execution(* com.book_loan_app.controller.BookManagerController.savePayBook(..))")
-    public void payBookPointCut() {
+    @Pointcut("execution(* com.book_loan_app.controller.BookManagerController.save*(..))")
+    public void BorrowPayBookPointCut() {
     }
 
-    @AfterReturning("payBookPointCut()")
+    @AfterReturning("BorrowPayBookPointCut()")
     public void payBookMethod(JoinPoint joinPoint) {
         System.out.println("mehtod name: "
                 + joinPoint.getSignature().getName()
