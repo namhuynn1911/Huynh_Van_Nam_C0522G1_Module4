a1bec2d1f6de660734af3210f1aee548b4598886
namhuynn1911 <namhuynh191193@gmail.com>
2022-09-30 21:22:57 +0700

    update

--- 07_spring_data_repository/exercise/01_blog/src/main/java/com/example_blog/controller/ApiBlogsController.java
+++ 07_spring_data_repository/exercise/01_blog/src/main/java/com/example_blog/controller/ApiBlogsController.java
@@ -17,7 +17,7 @@ public class ApiBlogsController {
     @Autowired
     private IBlogsService iBlogsService;
 
-    @GetMapping("/list")
+    @GetMapping("/v1")
     public ResponseEntity<List<Blogs>> showBlog() {
         List<Blogs> blogsList = iBlogsService.findAll();
         if (blogsList.isEmpty()) {
@@ -27,7 +27,7 @@ public class ApiBlogsController {
     }
 
 
-    @GetMapping("/{id}")
+    @GetMapping("v1/{id}")
     public ResponseEntity<Blogs> showView(@PathVariable int id) {
         Blogs blogs = iBlogsService.findById(id);
         if (blogs == null) {
@@ -36,7 +36,7 @@ public class ApiBlogsController {
         return new ResponseEntity<>(blogs, HttpStatus.OK);
     }
 
-    @GetMapping("/search")
+    @GetMapping("/v1")
     public ResponseEntity<List<Blogs>> showBlogByName(@RequestParam(value = "name") String name) {
         List<Blogs> blogsList = iBlogsService.findName(name);
         if (blogsList.isEmpty()) {
